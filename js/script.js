b692121db6f6b8b6b551585468dda853c2b8b467
YinaSolaris <yinan.zhan91@gmial.com>
2023-09-27 01:09:04 -0700

    STEP 6

--- js/script.js
+++ js/script.js
@@ -1,3 +1,52 @@
 //STEP 1
+/*
+let someMonth;
+function theMonth();
+let currentMonth;
+let summerMonth;
+let myLibraryFunction;
+*/
+
 //STEP 2
+/*
+4 // numeric literal
+'hello' // string literal
+false // boolean literal
+null // null literal
+*/
+
 //STEP 3
+/*
+let anExpression = 5 + 6 * 3;
+let secondExpression = 7 * 2 / 5 + 3;
+*/
+
+//STEP 4
+/*
+let firstName, lastName, address, city, state, zipCode, yourAge, referralSource, mayWeContactYou
+let strFirstName, strLastName, strAddress, strCity, strState, intZipCode, intYourAge, strReferralSource, blnMayWeContactYou
+/*
+
+//STEP 5
+/*
+let firstName = 'Yinan'
+let lastName = 'Zhan'
+let state = 'California'
+or:
+let firstName
+let lastName
+let state
+firstName = 'Yinan'
+lastName = 'Zhan'
+state = 'California'
+or:
+let firstName = 'Yinan', lastName = 'Zhan', state = 'California'
+/*
+
+//STEP 6
+/*
+let yearsLiving = 1
+alert('Yina has been living in SanDiego for ' + yearsLiving + ' years.')
+/*
+
+//STEP 7
